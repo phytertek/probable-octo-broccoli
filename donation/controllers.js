4a5fabab2d3eed42bb672a9121025a7f88a2a6c8
phytertek <ryan.phytertek@gmail.com>
2018-03-08 03:08:40 -0500

    donations test with split charge to customer

--- donation/controllers.js
+++ donation/controllers.js
@@ -4,52 +4,61 @@ const { requireFields } = require('enmapi/common/validation');
 const SSK = process.env.SSK;
 const stripe = require('stripe')(SSK);
 
+const COMMISION_PERCENTAGE = 5;
+const commission = amount => amount * COMMISION_PERCENTAGE / 100;
 module.exports = {
   postCreateDonation: async (req, res) => {
     try {
       const { token, donations } = req.body;
       requireFields({ token, donations });
-      const fields = ['firstName', 'lastName'];
       const user = req.unsafeUser;
-      fields.forEach(field => {
-        if (req.body[field]) user[field] = req.body[field];
+      // Create stripe customer if does not exist
+      if (!user.isDonor) {
+        ['firstName', 'lastName'].forEach(field => {
+          if (req.body[field]) user[field] = req.body[field];
+        });
+        const customer = await stripe.customers.create({
+          description: 'Donor',
+          source: token.id,
+          email: user.email
+        });
+        user.donorAcct = customer;
+        user.isDonor = true;
+      }
+      const newDonations = donations.map(d => {
+        return new Donation({
+          amount: d.amount,
+          fundraiser: d.fundraiser._id,
+          donor: user._id
+        });
       });
-      const customer = await stripe.customers.create({
-        description: 'Donor',
-        source: token.id,
-        email: user.email
+      const fundOwners = donations.map(d => {
+        return d.fundraiser.owner;
       });
-      console.log(customer);
-      res.json(customer);
+      let fundraiserAccts = await User.find({
+        _id: { $in: fundOwners }
+      }).select('fundraiserAcct');
+      fundraiserAccts = fundraiserAccts.reduce((fundRaiserAcctMap, owner) => {
+        fundRaiserAcctMap[owner._id] = owner.fundraiserAcct.stripe_user_id;
+        return fundRaiserAcctMap;
+      }, {});
+      const charges = newDonations.map(d =>
+        stripe.charges.create({
+          amount: d.amount,
+          currency: 'usd',
+          application_fee: commission(d.amount),
+          source: user.donorAcct.default_source,
+          destination: fundraiserAccts[d.owner],
+          customer: user.donorAcct.id
+        })
+      );
+      user.donations = [...user.donations, ...newDonations.map(d => d._id)];
+      await user.save();
+      await newDonations.save();
+      await Promise.all(charges);
+      res.json(charges);
     } catch (error) {
       sendUserError(error, res);
     }
   }
 };
-
-// const newDonations = donations.map(d => {
-//   return new Donation({
-//     amount: d.amount,
-//     fundraiser: d.fundraiser._id,
-//     donor: user._id
-//   })
-// })
-// const fundOwners = donations.map(d => {
-//   return d.fundraiser.owner
-// })
-// let fundraiserAccts = await User.find({ _id: { $in: fundOwners } }).select('fundraiserAcct')
-// let houseTake = 0
-// fundraiserAccts = fundraiserAccts.reduce((fundRaiserAcctMap, owner) => {
-//   fundRaiserAcctMap[owner._id] = owner.fundraiserAcct.stripe_user_id
-//   return fundRaiserAcctMap
-// }, {})
-// const chargesToMake = newDonations.map(d => {
-//   const amount = d.amount - (((d.amount* 100) * 5) / 100)
-// })
-// const charges = []
-// newDonations.forEach(d => {
-//   charges.push(stripe
-// })
-// const charge = await stripe.charges.create({});
-// await user.save();
-// res.json({ success: true });
